36fc58fd36b03fd7d77d90d3f12255f900c4c4d5
sizzlecar <763667966@qq.com>
2018-04-16 23:52:12 +0800

    leetCode 题目注释

--- src/com/sizzle/LeetCode/SumOfTwoNums/Solution.java
+++ src/com/sizzle/LeetCode/SumOfTwoNums/Solution.java
@@ -2,6 +2,12 @@ package com.sizzle.LeetCode.SumOfTwoNums;
 
 /**
  * Created by sizzle_carl on 2018/4/16.
+ *
+ * 给定一个整数数组和一个目标值，找出数组中和为目标值的两个数。
+ 你可以假设每个输入只对应一种答案，且同样的元素不能被重复利用。
+ 给定 nums = [2, 7, 11, 15], target = 9
+ 因为 nums[0] + nums[1] = 2 + 7 = 9
+ 所以返回 [0, 1]
  */
 public class Solution {
 
