64e82b314d94e6ad2e7aa06898e27a23a7ca4c92
sizzlecar <763667966@qq.com>
2018-08-05 10:27:53 +0800

    leetCode算法题八皇后问题

---
+++ src/com/sizzle/leetCode/nQueensPuzzle/Solution.java
@@ -0,0 +1,21 @@
+package com.sizzle.leetCode.nQueensPuzzle;
+
+/**
+ * Created by sizzle_carl on 2018/8/5.
+ * 皇后规则：横、直、斜都可以走，步数不受限制，但不能越子。
+ * n 皇后问题研究的是如何将 n 个皇后放置在 n×n 的棋盘上，并且使皇后彼此之间不能相互攻击。
+
+ 给定一个整数 n，返回所有不同的 n 皇后问题的解决方案。
+
+ 每一种解法包含一个明确的 n 皇后问题的棋子放置方案，该方案中 'Q' 和 '.' 分别代表了皇后和空位。
+ *
+ *
+ */
+
+
+
+
+public class Solution {
+
+
+}
