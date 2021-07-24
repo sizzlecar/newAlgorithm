20db4b99c6af7a3401054153b4fb71f2079906b1
carl.che@voyageone.cn <guantingting5479>
2021-07-25 00:23:24 +0800

    添加注释

--- src/main/java/com/sizzle/leet/code/reverse_integer/Solution.java
+++ src/main/java/com/sizzle/leet/code/reverse_integer/Solution.java
@@ -35,15 +35,26 @@ public class Solution {
 
     public int reverse(int x) {
         int result = 0;
+        //判断是否是负数
         boolean negative = x < 0;
+        //负数按正数处理
         if(negative) x = -x;
+        //32位有符号范围，正数最大值取反
         int limit = negative ? Integer.MIN_VALUE : -Integer.MAX_VALUE;
+        //最大值减少一个数量级
         int subLimit = limit / 10;
         while (x != 0){
+            //循环取x的个位数
             int currentInt = x % 10;
+            //1.如果x是负数, subLimit = -214748364, 如果 result < subLimit 再拼上一位也一定小于 Integer.MIN_VALUE,即超出范围
+            //2.如果x是正数, subLimit = -214748364, 如果 result < subLimit -> -result > -subLimit 再拼上一位也一定大于 Integer.MAX_VALUE,即超出范围
             if(result < subLimit) return 0;
+            //循环增加数量级
             result *= 10;
+            //1.如果x是负数, limit = -2147483648, 如果 result - currentInt < limit 小于 Integer.MIN_VALUE 超出范围
+            //2.如果x是正数, limit = -2147483647, 如果 -result + currentInt > limit 大于 Integer.MAX_VALUE 超出范围
             if(result < currentInt + limit) return 0;
+            //统一按负数处理
             result -= currentInt;
             x /= 10;
         }
